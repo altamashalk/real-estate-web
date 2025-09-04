94d4dadd5974f229833b22d3972be8ea0da6baf2
altamashalk <altamashk785@gmail.com>
2025-09-04 18:04:32 +0530

    Initial project commit

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,14 +3,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAM6n9BiLySA65m6M22QcgCmHT1VL01Xz4",
-  authDomain: "real-estate-app-fe1b2.firebaseapp.com",
-  projectId: "real-estate-app-fe1b2",
-  storageBucket: "real-estate-app-fe1b2.appspot.com",
-  messagingSenderId: "387431473399",
-  appId: "1:387431473399:web:18c7139a3ccff853b8f3f2",
-  measurementId: "G-09W4ZFXN3B",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export default app;
